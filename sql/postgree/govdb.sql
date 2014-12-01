b1b78ae2f5517845c44b5035d8f4e998be1274b8
Alex BC <alexbcbr@gmail.com>
2014-12-01 01:18:03 -0200

    scripts de postgre

---
+++ sql/postgree/govdb.sql
@@ -0,0 +1,6 @@
+--create table cidadao(id integer, nome varchar(30), endereco varchar(40));
+--insert into cidadao values(1, 'Alex', 'av paulista, 1000');
+--insert into cidadao values(2, 'Joao', 'av paulista, 1000');
+--insert into cidadao values(3, 'Maria', 'av paulista, 1000');
+--insert into cidadao values(4, 'Pedro', 'av paulista, 1000');
+select * from cidadao;
