da15725d006bda1b4c2d550423d1f3e36ffaacac
“AlexBC” <alexbcbr@gmail.com>
2014-12-07 13:45:42 -0200

    atualizar postgree

--- sql/postgree/govdb.sql
+++ sql/postgree/govdb.sql
@@ -2,5 +2,9 @@
 --insert into cidadao values(1, 'Alex', 'av paulista, 1000');
 --insert into cidadao values(2, 'Joao', 'av paulista, 1000');
 --insert into cidadao values(3, 'Maria', 'av paulista, 1000');
---insert into cidadao values(4, 'Pedro', 'av paulista, 1000');
+--insert into cidadao values(6, 'Pedro-bi', 'av paulista, 1000');
+
+
+delete from cidadao where id < 5;
+
 select * from cidadao;
