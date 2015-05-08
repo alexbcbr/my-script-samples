e3d557bf93085d51435527492d50f8eb754c6b3e
“AlexBC” <alexbcbr@gmail.com>
2015-05-08 15:00:42 -0300

    teste de git

--- python/word.py
+++ python/word.py
@@ -1,13 +1,14 @@
+
 #!/usr/bin/python
-import sys 
-import re 
+import sys
+import re
 
-def main(argv): 
-    pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*") 
-    for line in sys.stdin: 
-        for word in pattern.findall(line): 
-            print "LongValueSum:" + word.lower() + "\t" + "1" 
+def main(argv):
+    pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*")
+    for line in sys.stdin:
+        for word in pattern.findall(line):
+            print "LongValueSum:" + word.lower() + "\t" + "1"
 
 
-if __name__ == "__main__": 
-    main(sys.argv) 
+if __name__ == "__main__":
+    main(sys.argv)
